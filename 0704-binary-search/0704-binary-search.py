345716247470d2280f842b5c5495a7235be7d217
SAJID <xtremesajid58@gmail.com>
2026-06-29 09:37:09 +0530

    Time: 0 ms (100%), Space: 20.6 MB (8.53%) - LeetHub

---
+++ 0704-binary-search/0704-binary-search.py
@@ -0,0 +1,16 @@
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        p=0
+        q=len(nums)-1
+        
+
+        while (p<=q):
+            mid = (p+q)//2
+            if nums[mid]== target:
+                return mid
+            elif nums[mid]<target:
+                p=mid+1
+            else :
+                q=mid-1
+        return -1
+            
