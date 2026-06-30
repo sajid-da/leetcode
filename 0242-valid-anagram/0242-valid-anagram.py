f10b2d3cc8ff84d1c5512cef32b1465ab1f9dcb3
SAJID <xtremesajid58@gmail.com>
2026-06-30 19:48:29 +0530

    Time: 20 ms (5.8%), Space: 20.3 MB (9.75%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -1,6 +1,4 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        if sorted(s) == sorted(t):
-            return True
-        else:
-            return False
+        return sorted(s) == sorted(t)
+            
